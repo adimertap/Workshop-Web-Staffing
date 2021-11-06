ace1bb2464133539f189fa89adf311f2815f5dc9
adimertaaa <adimerta@student.unud.ac.id>
2021-11-06 15:41:09 +0800

    gg

--- resources/views/pages/kepegawaian/jadwal/jadwal.blade.php
+++ resources/views/pages/kepegawaian/jadwal/jadwal.blade.php
@@ -379,7 +379,22 @@
                             
                         ]
                     })
-                    alert('Berhasil Menghapus Jadwal')
+                    const Toast = Swal.mixin({
+                        toast: true,
+                        position: 'top-end',
+                        showConfirmButton: false,
+                        timer: 3000,
+                        timerProgressBar: true,
+                        didOpen: (toast) => {
+                            toast.addEventListener('mouseenter', Swal.stopTimer)
+                            toast.addEventListener('mouseleave', Swal.resumeTimer)
+                        }
+                    })
+
+                    Toast.fire({
+                        icon: 'success',
+                        title: 'Berhasil Menghapus Data Jadwal Pegawai'
+                    })
                    makecalendar()
                 },
                 error: function (response) {
