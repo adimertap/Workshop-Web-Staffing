254b8fdd350de461882719aa9f4b86c9ccfa0b70
adimertaaa <adimerta@student.unud.ac.id>
2021-11-07 15:32:16 +0800

    gg

--- app/Model/SingleSignOn/Role.php
+++ app/Model/SingleSignOn/Role.php
@@ -2,6 +2,7 @@
 
 namespace App\Model\SingleSignOn;
 
+use App\User;
 use Illuminate\Database\Eloquent\Model;
 
 class Role extends Model
@@ -10,7 +11,12 @@ class Role extends Model
 
     protected $primaryKey = 'id_role';
 
-    protected $fillable = ['role'];
+    protected $fillable = ['role','id_user'];
 
     public $timestamps = false;
+
+    public function user()
+    {
+        return $this->belongsTo(User::class, 'id', 'id_user');
+    }
 }
