a467076dc588a84e1bdeb553736bab118d770809
adimertaaa <adimerta@student.unud.ac.id>
2021-12-21 00:30:44 +0800

    gg

--- resources/views/pages/kepegawaian/masterdata/pegawai/edit.blade.php
+++ resources/views/pages/kepegawaian/masterdata/pegawai/edit.blade.php
@@ -65,6 +65,17 @@
                                     <div class="form-row">
                                         <div class="form-group col-md-6">
                                             <div class="row" id="radio1">
+
+                                                @if ($item->status_cabang == 'Pegawai Cabang')
+                                                    <div class="col-md-6">
+                                                        <input class="mr-1" value="Tidak Pegawai Cabang" type="radio"
+                                                            name="status_cabang">Tidak Pegawai Cabang
+                                                    </div>
+                                                    <div class="col-md-6">
+                                                        <input class="mr-1" value="Pegawai Cabang" type="radio"
+                                                            name="status_cabang" checked>Pegawai Cabang
+                                                    </div>
+                                                @else
                                                 <div class="col-md-6">
                                                     <input class="mr-1" value="Tidak Pegawai Cabang" type="radio"
                                                         name="status_cabang" checked>Tidak Pegawai Cabang
@@ -73,16 +84,16 @@
                                                     <input class="mr-1" value="Pegawai Cabang" type="radio"
                                                         name="status_cabang">Pegawai Cabang
                                                 </div>
+                                                @endif
+
+                                              
                                             </div>
                                         </div>
                                     </div>
                                     <div class="form-row">
-                                        <div class="form-group col-md-6">
+                                        <div class="form-group col-md-6" id="PegawaiCabangs" style="display:none">
                                             <label class="small mb-1" for="id_cabang">Cabang</label>
                                             <select class="form-control" name="id_cabang" id="id_cabang" required>
-                                                
-                                                
-
                                                 <option value="{{ $item->cabang->id_cabang ?? '' }}">{{ $item->cabang->nama_cabang ?? '' }}</option>
                                                 @foreach ($cabang as $cabangs)
                                                     <option value="{{ $cabangs->id_cabang }}">{{ $cabangs->nama_cabang }}
