ddea599a45a082b2c6bf1e93c119c4757eb2bc15
adimertaaa <adimerta@student.unud.ac.id>
2021-12-21 00:13:49 +0800

    gg

--- resources/views/pages/kepegawaian/masterdata/pegawai/detail.blade.php
+++ resources/views/pages/kepegawaian/masterdata/pegawai/detail.blade.php
@@ -233,6 +233,21 @@
                                                         {{ $item->tanggal_masuk }}
                                                 </div>
                                             </div>
+                                            <div class="row">
+                                                <div class="col-md-4">
+                                                    <div class="d-flex flex-column font-weight-bold">
+                                                        <label class="small text-muted line-height-normal">Penempatan
+                                                    </div>
+                                                </div>
+                                                <div class="col">
+                                                    <label class="small text-muted line-height-normal">:
+                                                        @if ($item->status_cabang == 'Pegawai Cabang')
+                                                            {{ $item->cabang->nama_cabang }}
+                                                        @else
+                                                            Pegawai Pusat
+                                                        @endif
+                                                </div>
+                                            </div>
                                         </form>
                                     </div>
                                 </div>
