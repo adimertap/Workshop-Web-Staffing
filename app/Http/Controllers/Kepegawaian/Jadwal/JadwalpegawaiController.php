983c51728a7d95166bfef160e4f6456adf6738ba
adimertaaa <adimerta@student.unud.ac.id>
2021-12-21 10:21:53 +0800

    gg

--- app/Http/Controllers/Kepegawaian/Jadwal/JadwalpegawaiController.php
+++ app/Http/Controllers/Kepegawaian/Jadwal/JadwalpegawaiController.php
@@ -28,10 +28,6 @@ class JadwalpegawaiController extends Controller
 
         $today = Carbon::now()->isoFormat('dddd');
         $tanggal = Carbon::now()->format('j F Y');
-        $id_pegawai = Pegawai::join('tb_kepeg_master_jabatan', 'tb_kepeg_master_pegawai.id_jabatan', 'tb_kepeg_master_jabatan.id_jabatan')
-        ->where('nama_jabatan', '!=', 'Owner')->pluck('id_pegawai','id_cabang')->toArray();
-        return $id_pegawai;
-       
 
         return view('pages.kepegawaian.jadwal.jadwal', compact('today', 'tanggal','bengkel'));
     }
@@ -43,22 +39,23 @@ class JadwalpegawaiController extends Controller
         $id_pegawai = Pegawai::join('tb_kepeg_master_jabatan', 'tb_kepeg_master_pegawai.id_jabatan', 'tb_kepeg_master_jabatan.id_jabatan')
         ->where('nama_jabatan', '!=', 'Owner')->pluck('id_pegawai')->toArray();
 
+        $status_cabang = Auth::user()->pegawai->status_cabang;
        
 
-        // if($status_cabang == 'Pegawai Cabang'){
-        //     $pegawaimasuk = Pegawai::leftJoin('tb_kepeg_jadwal', 'tb_kepeg_master_pegawai.id_pegawai', 'tb_kepeg_jadwal.id_pegawai')
-        //     ->join('tb_kepeg_master_jabatan', 'tb_kepeg_master_pegawai.id_jabatan', 'tb_kepeg_master_jabatan.id_jabatan')
-        //     ->select('tb_kepeg_master_pegawai.id_pegawai', 'nama_pegawai','nama_jabatan','tanggal_jadwal')
-        //     ->whereIn('tb_kepeg_master_pegawai.id_pegawai', $id_pegawai)
-        //     ->whereDate('tanggal_jadwal', $request->date)
-        //     ->where('tb_kepeg_master_pegawai.id_cabang', Auth::user()->pegawai->cabang->id_cabang);
-        // }else{
-        //      $pegawaimasuk = Pegawai::leftJoin('tb_kepeg_jadwal', 'tb_kepeg_master_pegawai.id_pegawai', 'tb_kepeg_jadwal.id_pegawai')
-        //     ->join('tb_kepeg_master_jabatan', 'tb_kepeg_master_pegawai.id_jabatan', 'tb_kepeg_master_jabatan.id_jabatan')
-        //     ->select('tb_kepeg_master_pegawai.id_pegawai', 'nama_pegawai','nama_jabatan','tanggal_jadwal')
-        //     ->whereIn('tb_kepeg_master_pegawai.id_pegawai', $id_pegawai)
-        //     ->whereDate('tanggal_jadwal', $request->date);
-        // }
+        if($status_cabang == 'Pegawai Cabang'){
+            $pegawaimasuk = Pegawai::leftJoin('tb_kepeg_jadwal', 'tb_kepeg_master_pegawai.id_pegawai', 'tb_kepeg_jadwal.id_pegawai')
+            ->join('tb_kepeg_master_jabatan', 'tb_kepeg_master_pegawai.id_jabatan', 'tb_kepeg_master_jabatan.id_jabatan')
+            ->select('tb_kepeg_master_pegawai.id_pegawai', 'nama_pegawai','nama_jabatan','tanggal_jadwal')
+            ->whereIn('tb_kepeg_master_pegawai.id_pegawai', $id_pegawai)
+            ->whereDate('tanggal_jadwal', $request->date)
+            ->where('tb_kepeg_master_pegawai.id_cabang', Auth::user()->pegawai->cabang->id_cabang);
+        }else{
+             $pegawaimasuk = Pegawai::leftJoin('tb_kepeg_jadwal', 'tb_kepeg_master_pegawai.id_pegawai', 'tb_kepeg_jadwal.id_pegawai')
+            ->join('tb_kepeg_master_jabatan', 'tb_kepeg_master_pegawai.id_jabatan', 'tb_kepeg_master_jabatan.id_jabatan')
+            ->select('tb_kepeg_master_pegawai.id_pegawai', 'nama_pegawai','nama_jabatan','tanggal_jadwal')
+            ->whereIn('tb_kepeg_master_pegawai.id_pegawai', $id_pegawai)
+            ->whereDate('tanggal_jadwal', $request->date);
+        }
        
      
         // $pegawaimasuk = Pegawai::leftJoin('tb_kepeg_jadwal', 'tb_kepeg_master_pegawai.id_pegawai', 'tb_kepeg_jadwal.id_pegawai')
