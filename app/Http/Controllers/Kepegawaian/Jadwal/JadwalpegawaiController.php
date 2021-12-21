8aff6a2d8c769c3c6a46a72d2eeb21cd5873ed5a
adimertaaa <adimerta@student.unud.ac.id>
2021-12-21 10:26:13 +0800

    gg

--- app/Http/Controllers/Kepegawaian/Jadwal/JadwalpegawaiController.php
+++ app/Http/Controllers/Kepegawaian/Jadwal/JadwalpegawaiController.php
@@ -53,8 +53,9 @@ class JadwalpegawaiController extends Controller
         }else{
              $pegawaimasuk = Pegawai::leftJoin('tb_kepeg_jadwal', 'tb_kepeg_master_pegawai.id_pegawai', 'tb_kepeg_jadwal.id_pegawai')
             ->join('tb_kepeg_master_jabatan', 'tb_kepeg_master_pegawai.id_jabatan', 'tb_kepeg_master_jabatan.id_jabatan')
-            ->select('tb_kepeg_master_pegawai.id_pegawai', 'nama_pegawai','nama_jabatan','tanggal_jadwal')
+            ->select('tb_kepeg_master_pegawai.id_pegawai', 'nama_pegawai','nama_jabatan','tanggal_jadwal','id_cabang')
             ->whereIn('tb_kepeg_master_pegawai.id_pegawai', $id_pegawai)
+            ->where('tb_kepeg_master_pegawai.id_cabang', Auth::user()->pegawai->cabang->id_cabang)
             ->whereDate('tanggal_jadwal', $request->date);
         }
        
